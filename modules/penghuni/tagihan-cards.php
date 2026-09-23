aef7d0c13eb7c9e7bae4a4d8cc33ee25fcf0aec0
pacifiista <naurasfh2020@gmail.com>
2026-09-23 14:44:50 +0700

    update nawra

--- modules/penghuni/tagihan-cards.php
+++ modules/penghuni/tagihan-cards.php
@@ -16,6 +16,27 @@ function formatTanggal(?string $tanggal): string {
     if (!$tanggal) return '-';
      $bulan = [1=>'January',2=>'February',3=>'March',4=>'April',5=>'May',6=>'June',
               7=>'July',8=>'August',9=>'September',10=>'October',11=>'November',12=>'December'];
-        
-    
+        $ts = strtotime($tanggal);
+    return date('D', $ts) . ', ' . date('d', $ts) . ' ' . $bulan[(int)date('n', $ts)] . ' ' . date('Y', $ts);  
 }
+
+?>
+<div class="page-content">
+     <?php foreach (['sewa' => 'Pembayaran Sewa', 'iuran' => 'Pembayaran Iuran'] as $jenis => $label): ?>
+        <div class="tagihan-card">
+            <span class="label"><?= $label ?></span>
+            <div class="row">
+                <span>Status Pembayaran</span>
+                <?php $t = $tagihan[$jenis]; ?>
+                <?php if ($t && $t['status'] === 'lunas'): ?>
+                    <span class="status-lunas">Lunas</span>
+                <?php else: ?>
+                    <span class="status-belum">Belum Lunas</span>
+                <?php endif; ?>
+            </div>
+            <div class="row">
+                <span>Tenggat Pembayaran</span>
+                <span><?= formatTanggal($t['tanggal_jatuh_tempo'] ?? null) ?></span>
+            </div>
+        </div>
+    <?php endforeach; ?>
